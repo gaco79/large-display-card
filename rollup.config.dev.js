fda3a1debf40c2aec87e639f59935e3f5a5f01ba
Gareth Cooper <gareth@garethcooper.com>
2025-10-02 14:09:10 +0100

    feat: add rollup-plugin-livereload for live reloading during development

--- rollup.config.dev.js
+++ rollup.config.dev.js
@@ -3,6 +3,8 @@ import babel from '@rollup/plugin-babel';
 import terser from '@rollup/plugin-terser';
 import resolve from '@rollup/plugin-node-resolve';
 import json from '@rollup/plugin-json';
+import livereload from 'rollup-plugin-livereload';
+import { watch } from 'fs';
 
 export default {
   input: ['src/large-display-card.ts'],
@@ -18,5 +20,10 @@ export default {
       exclude: 'node_modules/**',
     }),
     terser(),
+    livereload('dist'),
   ],
+  watch: {
+    chokidar: true,
+    exclude: ['node_modules/**', 'dist/**', '.vscode/**', '.git/**', 'config/**'],
+  }
 };
